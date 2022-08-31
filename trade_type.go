5c69495b0e27ea959f48dd19fc90e03c6dfd6c31
lersoua <lerosua@gmail.com>
2022-08-31 17:21:36 +0800

    丰富 SettleDetailInfo 类型

--- trade_type.go
+++ trade_type.go
@@ -657,8 +657,17 @@ type TradeOrderInfoSyncRsp struct {
 }
 
 type SettleDetailInfo struct {
-	Amount      string `json:"amount"`
+	Amount      string `json:"amount"` //结算的金额，单位为元。在创建订单和支付接口时必须和交易金额相同。在结算确认接口时必须等于交易金额减去已退款金额。
 	TransInType string `json:"trans_in_type"`
+	/*** 结算收款方的账户类型。cardAliasNo：结算收款方的银行卡编号;
+	userId：表示是支付宝账号对应的支付宝唯一用户号;
+	loginName：表示是支付宝登录号；
+	defaultSettle：表示结算到商户进件时设置的默认结算账号，结算主体为门店时不支持传defaultSettle；
+	***/
+	TransIn          string `json:"trans_in,omitempty"`           //结算收款方。当结算收款方类型是cardAliasNo时，本参数为用户在支付宝绑定的卡编号；结算收款方类型是userId时，本参数为用户的支付宝账号对应的支付宝唯一用户号，以2088开头的纯16位数字；当结算收款方类型是loginName时，本参数为用户的支付宝登录号；当结算收款方类型是defaultSettle时，本参数不能传值，保持为空。
+	SummaryDimension string `json:"summary_dimension,omitempty"`  //结算汇总维度，按照这个维度汇总成批次结算，由商户指定。 目前需要和结算收款方账户类型为cardAliasNo配合使用
+	SettleEntityId   string `json:"settle_entity_id,omitempty"`   //结算主体标识。当结算主体类型为SecondMerchant时，为二级商户的SecondMerchantID；当结算主体类型为Store时，为门店的外标。
+	SettleEntityType string `json:"settle_entity_type,omitempty"` //结算主体类型。 二级商户:SecondMerchant;商户或者直连商户门店:Store
 }
 type SettleInfo struct {
 	SettleDetailInfos []*SettleDetailInfo `json:"settle_detail_infos"`
